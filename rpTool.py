e7a00349a7bd2ceca8078ca3d077f933c75689d2
mdulac <melchior.dulac@inra.fr>
2020-01-17 20:39:51 +0100

    using the rest version

--- rpTool.py
+++ rpTool.py
@@ -13,18 +13,24 @@ import glob
 
 MAX_VIRTUAL_MEMORY = 20000 * 1024 * 1024 # 20GB -- define what is the best
 
+##
+#
+#
 def limit_virtual_memory():
     resource.setrlimit(resource.RLIMIT_AS, (MAX_VIRTUAL_MEMORY, resource.RLIM_INFINITY))
 
-def run(rp2_pathways_bytes, timeout):
+##
+#
+#
+def run_rp2paths(rp2_pathways_bytes, timeout):
     out_paths = b''
     out_compounds = b''
-    with tempfile.TemporaryDirectory() as tmpFolder:
-        rp2_pathways = tmpFolder+'/tmp_rp2_pathways.csv'
-        with open(tmpFolder+'/tmp_rp2_pathways.csv', 'wb') as outfi:
+    with tempfile.TemporaryDirectory() as tmpOutputFolder:
+        rp2_pathways = tmpOutputFolder+'/tmp_rp2_pathways.csv'
+        with open(tmpOutputFolder+'/tmp_rp2_pathways.csv', 'wb') as outfi:
             outfi.write(rp2_pathways_bytes)
-        #rp2paths_command = ['python', '/home/RP2paths.py', 'all', rp2_pathways, '--outdir', tmpFolder, '--timeout', str(timeout)]
-        rp2paths_command = 'python /data/RP2paths.py all '+str(rp2_pathways)+' --outdir '+str(tmpFolder)+' --timeout '+str(timeout)
+        #rp2paths_command = ['python', '/home/RP2paths.py', 'all', rp2_pathways, '--outdir', tmpOutputFolder, '--timeout', str(timeout)]
+        rp2paths_command = 'python /data/RP2paths.py all '+str(rp2_pathways)+' --outdir '+str(tmpOutputFolder)+' --timeout '+str(timeout)
         try:
             commandObj = subprocess.Popen(rp2paths_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, preexec_fn=limit_virtual_memory)
             #commandObj = subprocess.Popen(rp2paths_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=False, preexec_fn=limit_virtual_memory)
@@ -38,9 +44,9 @@ def run(rp2_pathways_bytes, timeout):
                 return b'', b'', b'memoryerror'
             ### convert the result to binary and return ###
             try:
-                with open(tmpFolder+'/out_paths.csv', 'rb') as op:
+                with open(tmpOutputFolder+'/out_paths.csv', 'rb') as op:
                     out_paths = op.read()
-                with open(tmpFolder+'/compounds.txt', 'rb') as c:
+                with open(tmpOutputFolder+'/compounds.txt', 'rb') as c:
                     out_compounds = c.read()
                 return out_paths, out_compounds, b''
             except FileNotFoundError:
@@ -53,3 +59,31 @@ def run(rp2_pathways_bytes, timeout):
             logging.error('Cannot set the RAM usage limit')
             return b'', b'', b'valueerror'
     return out_paths, out_compounds, b''
+
+
+
+##
+#
+#
+def main(rp2_pathways, timeout):
+    with open(rp2_pathways, 'rb') as rp2_pathways_bytes:
+        result = run_rp2paths(rp2_pathways_bytes, timeout)
+        #app.logger.info(result)
+        if result[2]==b'filenotfounderror':
+            logging.error('FileNotFound Error from rp2paths')
+            return False
+        elif result[2]==b'oserror':
+            logging.error('rp2paths has generated an OS error')
+            return False
+        elif result[2]==b'memerror':
+            logging.error(' Memory allocation error')
+            return False
+        elif result[0]==b'' and result[1]==b'':
+            logging.error('Could not find any results by RetroPath2.0')
+            return False
+        elif result[2]==b'valueerror':
+            logging.error('Could not setup a RAM limit')
+            return False
+        out_paths = io.BytesIO(result[0])
+        out_compounds = io.BytesIO(result[1])
+        return out_paths, out_compounds
