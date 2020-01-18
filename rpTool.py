98ce0f03b571b84856ae0342143ddebf553e3908
mdulac <melchior.dulac@inra.fr>
2020-01-18 20:24:17 +0100

    updated rpTool

--- rpTool.py
+++ rpTool.py
@@ -10,6 +10,7 @@ import resource
 import tempfile
 import glob
 import io
+import logging
 
 MAX_VIRTUAL_MEMORY = 20000 * 1024 * 1024 # 20GB -- define what is the best
 #MAX_VIRTUAL_MEMORY = 20 * 1024 * 1024 # 20GB -- define what is the best
@@ -23,15 +24,18 @@ def limit_virtual_memory():
 ##
 #
 #
-def run_rp2paths(rp2_pathways_bytes, timeout, logger):
+def run_rp2paths(rp2_pathways_bytes, timeout, logger=None):
+    if logger==None:
+        logging.basicConfig(level=logging.DEBUG)
+        logger = logging.getLogger(__name__)
     out_paths = b''
     out_compounds = b''
     with tempfile.TemporaryDirectory() as tmpOutputFolder:
         rp2_pathways = tmpOutputFolder+'/tmp_rp2_pathways.csv'
         with open(tmpOutputFolder+'/tmp_rp2_pathways.csv', 'wb') as outfi:
             outfi.write(rp2_pathways_bytes)
-        #rp2paths_command = ['python', '/home/RP2paths.py', 'all', rp2_pathways, '--outdir', tmpOutputFolder, '--timeout', str(timeout)]
-        rp2paths_command = 'python /home/RP2paths.py all '+str(rp2_pathways)+' --outdir '+str(tmpOutputFolder)+'/ --timeout '+str(int(timeout*60.0*60.0+10.0))
+        #rp2paths_command = ['python', 'rp2paths-1.0.2/RP2paths.py', 'all', rp2_pathways, '--outdir', tmpOutputFolder, '--timeout', str(timeout)]
+        rp2paths_command = 'python /home/RP2paths.py all '+str(rp2_pathways)+' --outdir '+str(tmpOutputFolder)+' --timeout '+str(int(timeout*60.0+10.0))
         try:
             commandObj = subprocess.Popen(rp2paths_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, preexec_fn=limit_virtual_memory)
             #commandObj = subprocess.Popen(rp2paths_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=False, preexec_fn=limit_virtual_memory)
