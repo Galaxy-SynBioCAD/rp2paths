d5306a42fdad726021e6c965d7ca9ceef04f9c14
mdulac <melchior.dulac@inra.fr>
2020-09-19 20:21:56 +0200

    updated the subprocess call

--- rpTool.py
+++ rpTool.py
@@ -42,16 +42,15 @@ def run_rp2paths(rp2_pathways, timeout, max_steps=0, max_paths=150, unfold_compo
         timeout_rest = int(timeout*60.0+10.0)
         if timeout>120:
             timeout_rest = 120
-        rp2paths_command = 'python /home/RP2paths.py all '+str(rp2_pathways)+' --outdir '+str(tmpOutputFolder)+' --timeout '+str(int(timeout_rest*60.0))+' --max_steps '+str(max_steps)+' --maxpaths '+str(max_paths)+' --unfold_compounds '+str(unfold_compounds)
+        rp2paths_command = 'python /home/RP2paths.py all '+str(rp2_pathways)+' --outdir '+str(tmpOutputFolder)+' --timeout '+str(int(timeout*60.0))+' --max_steps '+str(max_steps)+' --maxpaths '+str(max_paths)+' --unfold_compounds '+str(unfold_compounds)
         try:
-            commandObj = subprocess.Popen(rp2paths_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, preexec_fn=limit_virtual_memory)
+            commandObj = subprocess.Popen(rp2paths_command.split(' '), stdout=subprocess.PIPE, stderr=subprocess.PIPE preexec_fn=limit_virtual_memory)
             try:
-                commandObj.wait(timeout=timeout_rest*60.0)
+                result, error = commandObj.communicate(timeout=timeout*60.0+10.0)
             except subprocess.TimeoutExpired as e:
-                logger.error('Timeout from rp2paths ('+str(timeout)+' minutes)')
+                logger.error('Timeout from of ('+str(timeout)+' minutes)')
                 commandObj.kill()
                 return b'', b'', b'timeout', str.encode('Command: '+str(rp2paths_command)+'\n Error: '+str(e)+'\n tmpfolder: '+str(glob.glob(tmpfolder+'/*')))
-            (result, error) = commandObj.communicate()
             result = result.decode('utf-8')
             error = error.decode('utf-8')
             #TODO test to see what is the correct phrase
